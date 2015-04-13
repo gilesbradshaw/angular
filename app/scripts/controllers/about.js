a65e2f5eab8f4f8658885fc7524f0fb1397b6245
giles <giles.bradshaw@sigyl.com>
2015-04-13 18:46:09 +0100

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name abrmsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the abrmsApp
+ */
+angular.module('abrmsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name abrmsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the abrmsApp
+ */
+angular.module('abrmsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
